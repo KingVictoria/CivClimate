209431b0daf2290488a4255911f7a1a63b971e36
KingVictoria <TheRealKingVictoria@gmail.com>
2018-06-27 16:38:22 -0400

    initial commit

---
+++ src/main/java/com/gmail/therealkingvictoria/CivClimate.java
@@ -0,0 +1,15 @@
+package com.gmail.therealkingvictoria;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CivClimate extends JavaPlugin {
+    
+	public void onEnable() {
+		
+	} // onEnable
+	
+	public void onDisable() {
+		
+	} // onDisable
+	
+} // class
